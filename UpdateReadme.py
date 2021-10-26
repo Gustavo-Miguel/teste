d74642134d7f724229dcb1e8af4e81b60c183010
Gustavo Miguel <33351250+Gustavo-Miguel@users.noreply.github.com>
2021-10-25 22:24:29 -0300

    Update UpdateReadme.py

--- UpdateReadme.py
+++ UpdateReadme.py
@@ -1,24 +1,30 @@
-
 import os
-from git import Repo
+import git 
 
 git_url = "https://github.com/Gustavo-Miguel/teste.git"
 repo_dir = "C://Users//Gustavo//Desktop//teste"
 
+def get_description():
+    f = open("C://Users//Gustavo//Desktop//description.txt", "r")
+    description = f.read()
+    return description
+
 if os.path.exists(repo_dir):
-    os.remove(repo_dir)
+    cloned_repo = git.Repo.init(repo_dir)
+    #os.remove(repo_dir)
     print("Directory already exist!")
 else:
+    cloned_repo = git.Repo.clone_from(git_url, repo_dir)
     print("Directory don't exist!")
 
-cloned_repo = Repo.clone_from(git_url, repo_dir)
+desc = get_description()
 
-f = open("C://Users//Gustavo//Desktop//teste//README.md", "a")
-f.write("\n\nNow the file has more content!\n\n")
+f = open("C://Users//Gustavo//Desktop//teste//CHANGELOG.md", "a")
+f.write(desc)
 f.close()
 
-cloned_repo.index.add("README.md")
-cloned_repo.index.commit("Append Readme")
+cloned_repo.index.add("*")
+cloned_repo.index.commit("Changelog commit")
 origin = cloned_repo.remote(name='origin')
 origin.push()
 
